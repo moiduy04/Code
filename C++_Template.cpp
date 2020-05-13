bad73de0285a6ca9694f89e336134ba1a2bfedc0
moiduy04 <65279552+moiduy04@users.noreply.github.com>
2020-05-13 18:20:50 +0700

    Update and rename A_C++_Template.cpp to C++_Template.cpp

---
+++ C++_Template.cpp
@@ -0,0 +1,51 @@
+//	Totally unrelated and not AT ALL stolen from hohohaha's template
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC target("popcnt")
+#pragma GCC optimize("03")
+#define arctan atan
+#define arccos acos
+#define arcsin asin
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define vt vector
+#define pr pair
+#define endl "\n"
+//	Until next time, remember:
+//			You are closer than you think.
+//						_Dan Lok_
+
+typedef double db;			typedef long long li;		typedef long double ld;			typedef string str;
+typedef pr<int, int> ii;	typedef pr<li,li> ll;		typedef pr<ld,ld> dd;
+typedef vt<int> vi;			typedef vt<li> vli;			typedef vt<bool> vb;
+typedef vt<str> vs;			typedef vt<ii> vii;			typedef vt<ll> vll;
+typedef map<int, int> mii;	typedef map<int, bool> mib;	typedef map<int, str> mis;		typedef map<int, char> mic;
+typedef set<int> s_i;		typedef set<char> s_c;		typedef set<str> s_s;
+typedef complex<db> cpx;	typedef vt<cpx> vcpx;
+const int mod = 1e9+7;		const li inf = 9223372036854775807;
+#define pi 3.1415926535897932	//faster than pi = acos(-1); but sometimes doesn't work
+//	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+//	srand(chrono::steady_clock::now().time_since_epoch().count());
+//	freopen("input.txt","r",stdin);
+
+void input()
+{
+	
+}
+
+int main()
+{
+//	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+//	freopen("FILE.inp","r",stdin); freopen("FILE.out","w",stdout);
+//	freopen("input.txt","r",stdin);
+	input();
+	return 0;
+}
+// with the aura of Dora, AC!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!!!!!!!!!!!!!!!
+// Why is there a one in my row of exclamation marks?
+
