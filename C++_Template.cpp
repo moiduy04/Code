e9022cfaabb434968ec0e3f119b0831adcd68f2a
moiduy04 <65279552+moiduy04@users.noreply.github.com>
2020-05-22 10:42:48 +0700

    Update C++_Template.cpp

--- C++_Template.cpp
+++ C++_Template.cpp
@@ -1,8 +1,15 @@
 //	Totally unrelated and not AT ALL stolen from hohohaha's template
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+#include <ext/rope>
+
 using namespace std;
-#pragma GCC target("popcnt")
+
 #pragma GCC optimize("03")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
 #define arctan atan
 #define arccos acos
 #define arcsin asin
@@ -15,19 +22,19 @@ using namespace std;
 #define ub upper_bound
 #define vt vector
 #define pr pair
+#define u_map unordered_map
 #define endl "\n"
 //	Until next time, remember:
 //			You are closer than you think.
 //						_Dan Lok_
 
-typedef double db;			typedef long long li;		typedef long double ld;			typedef string str;
-typedef pr<int, int> ii;	typedef pr<li,li> ll;		typedef pr<ld,ld> dd;
-typedef vt<int> vi;			typedef vt<li> vli;			typedef vt<bool> vb;
-typedef vt<str> vs;			typedef vt<ii> vii;			typedef vt<ll> vll;
+typedef double db;			typedef long long ll;		typedef long double ld;			typedef string str;
+typedef pr<int, int> pii;	typedef pr<ll,ll> pll;		typedef pr<ld,ld> pdd;
+typedef vt<int> vi;			typedef vt<ll> vll;			typedef vt<bool> vb;
+typedef vt<str> vs;			typedef vt<pii> vpii;		typedef vt<pll> vpll;
 typedef map<int, int> mii;	typedef map<int, bool> mib;	typedef map<int, str> mis;		typedef map<int, char> mic;
-typedef set<int> s_i;		typedef set<char> s_c;		typedef set<str> s_s;
 typedef complex<db> cpx;	typedef vt<cpx> vcpx;
-const int mod = 1e9+7;		const li inf = 9223372036854775807;
+//const int mod = 1e9+7;		const ll inf = 9223372036854775807;
 #define pi 3.1415926535897932	//faster than pi = acos(-1); but sometimes doesn't work
 //	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 //	srand(chrono::steady_clock::now().time_since_epoch().count());
@@ -35,10 +42,10 @@ const int mod = 1e9+7;		const li inf = 9223372036854775807;
 
 void input()
 {
-	
+
 }
 
-int main()
+signed main()
 {
 //	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 //	freopen("FILE.inp","r",stdin); freopen("FILE.out","w",stdout);
@@ -47,6 +54,4 @@ int main()
 	return 0;
 }
 // with the aura of Dora, AC!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!!!!!!!!!!!!!!!
-// Why is there a one in my row of exclamation marks?
-
-
+// Why is there node one in my row of exclamation marks?
