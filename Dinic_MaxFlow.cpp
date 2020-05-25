f08dd6bc5d7a0e8d661c59f8c0a08e604e42be07
moiduy04 <65279552+moiduy04@users.noreply.github.com>
2020-05-25 08:39:11 +0700

    Update Dinic_MaxFlow.cpp

--- Dinic_MaxFlow.cpp
+++ Dinic_MaxFlow.cpp
@@ -1,46 +1,45 @@
-struct edge{
+struct flow_edge{
 	int v,flow,cap,rev;
-	edge(const int& V, const int& passing_flow, 
+	flow_edge(const int& V, const int& passing_flow, 
 		 const int& capacity, const int& reverse_edge_index_in_adjlist)
 	{
 		v = V, flow = passing_flow, 
 		cap = capacity, rev = reverse_edge_index_in_adjlist;
 	}
 };
-ostream& operator << (ostream &stream, const edge& e){
-	return stream << "connected to node " << e.v << " with (" << (e.rev ? e.rev : e.flow) << '/' << e.cap << ')';
+ostream& operator << (ostream &stream, const flow_edge& e){
+	return stream << "connected to node " << e.v << " with (" << e.flow << '/' << e.cap << ')';
 }
-class graph{
+class flow_graph{
 	int n;
-	int *level;
+	vi level;
 public:
-	vt<edge> *adjlist;
-	graph(const int& N)
-	{
-		adjlist = new vt<edge>[N];
-		n = N;
-		level = new int[N];
-	}
+	vt<vt<flow_edge> > adjlist;
+	flow_graph(int N) {n = N, adjlist = vt<vt<flow_edge> >(N), level = vi(N);}
+	flow_graph(const vt<vi> &other)    {n = other.size(), adjlist = vt<vt<flow_edge> >(n), level = vi(n); for(int i = 0; i < other.size(); ++i) for(int j: other[i]) add(i,j);};
+	flow_graph(const vt<vpii> &other)  {n = other.size(), adjlist = vt<vt<flow_edge> >(n), level = vi(n); for(int i = 0; i < other.size(); ++i) for(pii j: other[i]) add(i,j.fi, j.se);};
+	flow_graph(const flow_graph &other){n = other.adjlist.size(), adjlist = other.adjlist, level = vi(n);}
+	flow_graph& operator= (const flow_graph& other){n = other.adjlist.size(), adjlist = other.adjlist, level = vi(n); return *this;}
 	
 	void add(const int &u, const int &v, const int& cap = 1,
 			const int& currflow = 0)
 	{
-		adjlist[u].pb(edge(v, currflow,cap,adjlist[v].size()));
-		adjlist[v].pb(edge(u,-currflow,0,adjlist[u].size()-1));
+		adjlist[u].pb(flow_edge(v, currflow,cap,adjlist[v].size()));
+		adjlist[v].pb(flow_edge(u,-currflow,0,adjlist[u].size()-1));
 	}
 	
 	queue<int> q;
 	bool bfs(const int& s, const int& t)
 	{
-		fill(level, level+n, -1);
+		fill(level.begin(), level.end(), -1);
 		q.push(s); level[s] = 0;
-		vt<edge>::iterator i;
+		vt<flow_edge>::iterator i;
 		while(!q.empty())
 		{
 			int u = q.front(); q.pop();
 			for(i = adjlist[u].begin(); i != adjlist[u].end(); ++i)
 			{
-				edge &e = *i;
+				flow_edge &e = *i;
 				if (level[e.v] < 0 && e.flow < e.cap)
 				{
 					level[e.v] = level[u] + 1;
@@ -58,7 +57,7 @@ public:
 		{
 			for(; node[u] < adjlist[u].size(); ++node[u])
 			{
-				edge &e = adjlist[u][node[u]];
+				flow_edge e = adjlist[u][node[u]];
 				if (level[e.v] == level[u]+1 && e.flow < e.cap)
 				{
 					int	temp = send_flow(e.v, t, node, 
