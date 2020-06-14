4d653f3ad980a257a0bfb2579dbb58c21e32ec1a
moiduy04 <65279552+moiduy04@users.noreply.github.com>
2020-06-14 18:16:55 +0700

    Update C++_Template.cpp

--- C++_Template.cpp
+++ C++_Template.cpp
@@ -7,15 +7,19 @@
 using namespace std;
 
 #pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 #define fi first
 #define se second
-
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define gcd __gcd
+#define  rep(i, end) for(int i = 0; i <  (end); ++i)
+#define rep1(i, end) for(int i = 1; i <= (end); ++i)
 #define lb lower_bound
 #define ub upper_bound
 
@@ -23,20 +27,23 @@ using namespace std;
 #define endl "\n"
 
 #define fileopen(filename) if (fopen(filename".INP", "r")) {freopen(filename".INP", "r", stdin);freopen(filename".OUT", "w", stdout);}
+#define local_input(x) if (x && fopen("input.txt","r")) freopen("input.txt","r",stdin);
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define rand_seed() srand(chrono::steady_clock::now().time_since_epoch().count())
 
 #define fscan(x)   scanf("%d",  &x)
+#define fscandb(x) scanf("%lf", &x)
 #define fscanll(x) scanf("%lld",&x)
 //	Until next time, remember:
 //			You are closer than you think.
 //						_Dan Lok_
 
-typedef double db;			typedef long long ll;		typedef long double ld;
-typedef pair<int, int> pii;	typedef pair<ll,ll> pll;	typedef pair<ld,ld> pdd;
-typedef vt<int> vi;			typedef vt<ll> vll;			typedef vt<bool> vb;
-typedef vt<string> vs;		typedef vt<pii> vpii;		typedef vt<pll> vpll;
-typedef complex<db> cpx;	typedef vt<cpx> vcpx;
+typedef double db;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<bool> vb;
 #define pi 3.1415926535897932	//faster than pi = acos(-1); but sometimes doesn't work?
 //	srand(chrono::steady_clock::now().time_since_epoch().count());
 //	freopen("input.txt","r",stdin);
@@ -45,7 +52,7 @@ typedef complex<db> cpx;	typedef vt<cpx> vcpx;
 
 signed main()
 {
-	fileopen(""); if (fopen("input.txt","r")) freopen("input.txt","r",stdin);
+	fileopen(""); local_input(true);
 	
 	return 0;
 }
