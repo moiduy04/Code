ae8b8822a0872af8555900e06dba0ccd9e4f2254
moiduy04 <65279552+moiduy04@users.noreply.github.com>
2020-06-15 12:52:07 +0700

    Update C++_Template.cpp

--- C++_Template.cpp
+++ C++_Template.cpp
@@ -18,6 +18,7 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 #define pb push_back
 #define eb emplace_back
 #define gcd __gcd
+#define rep(i,str,end) for(int i = (str), i < (end); ++i)
 #define  rep(i, end) for(int i = 0; i <  (end); ++i)
 #define rep1(i, end) for(int i = 1; i <= (end); ++i)
 #define lb lower_bound
